0aa18039485a246262a2757a84447af937b0ccf6
hsgamer <huynhqtienvtag@gmail.com>
2020-12-13 20:51:41 +0700

    update

--- src/main/java/me/hsgamer/bettergui/asciiplaceholders/Main.java
+++ src/main/java/me/hsgamer/bettergui/asciiplaceholders/Main.java
@@ -1,44 +1,42 @@
 package me.hsgamer.bettergui.asciiplaceholders;
 
+import me.hsgamer.bettergui.api.addon.BetterGUIAddon;
+import me.hsgamer.bettergui.lib.core.variable.VariableManager;
+import org.apache.commons.lang.StringEscapeUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
-import me.hsgamer.bettergui.manager.VariableManager;
-import me.hsgamer.bettergui.object.addon.Addon;
-import me.hsgamer.bettergui.util.MessageUtils;
-import org.apache.commons.lang.StringEscapeUtils;
 
-public final class Main extends Addon {
+public final class Main extends BetterGUIAddon {
 
-  private static final Map<String, String> placeholders = new HashMap<>();
+    private static final Map<String, String> placeholders = new HashMap<>();
 
-  @Override
-  public boolean onLoad() {
-    try {
-      saveResource("config.yml", false);
-      return true;
-    } catch (Exception e) {
-      getPlugin().getLogger().log(Level.WARNING, "Error when loading the addon", e);
-      return false;
+    @Override
+    public boolean onLoad() {
+        try {
+            saveResource("config.yml", false);
+            return true;
+        } catch (Exception e) {
+            getPlugin().getLogger().log(Level.WARNING, "Error when loading the addon", e);
+            return false;
+        }
     }
-  }
 
-  @Override
-  public void onEnable() {
-    registerASCII();
-    VariableManager.register("ascii_", ((player, s) -> placeholders.getOrDefault(s, null)));
-  }
+    @Override
+    public void onEnable() {
+        registerASCII();
+        VariableManager.register("ascii_", (original, uuid) -> placeholders.get(original));
+    }
 
-  @Override
-  public void onReload() {
-    placeholders.clear();
-    reloadConfig();
-    registerASCII();
-  }
+    @Override
+    public void onReload() {
+        placeholders.clear();
+        reloadConfig();
+        registerASCII();
+    }
 
-  private void registerASCII() {
-    getConfig().getValues(false).forEach((string, object) ->
-        placeholders.put(string,
-            MessageUtils.colorize(StringEscapeUtils.unescapeJava(String.valueOf(object)))));
-  }
+    private void registerASCII() {
+        getConfig().getValues(false).forEach((string, object) -> placeholders.put(string, StringEscapeUtils.unescapeJava(String.valueOf(object))));
+    }
 }
